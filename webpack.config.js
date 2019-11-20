fa6dc6f6163b83e9f574d9fbbe7be03969a79690
mohamed.fasil@inmobi.com <mohamed.fasil@inmobi.com>
2019-11-20 17:05:36 +0530

    Updating dependencies :fire:

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,24 @@
-const path = require('path');
+const path = require("path");
 
- module.exports = {
-   entry: {
-    bundle: './src/index.js'
+module.exports = {
+  mode: "production",
+  entry: {
+    bundle: "./src/index.js"
   },
-   output: {
-     path: path.resolve(__dirname),
-      filename: 'chi-square-p-value.js',
-      library: 'analyse',
-      libraryTarget: 'umd',
-      umdNamedDefine: true
-    },
+  output: {
+    path: path.resolve(__dirname),
+    filename: "chi-square-p-value.js",
+    library: "analyse",
+    libraryTarget: "umd",
+    umdNamedDefine: true
+  },
   module: {
-    rules: [{
-      test: /\.js$/,
-     exclude: /node_modules/,
-     use: 'babel-loader'
-   }]
- }
- };
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  }
+};
